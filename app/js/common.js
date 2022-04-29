1dcfe1c47179583bdf07cb4eaa61b14bd7379688
Viktoria <zaharova181114@gmail.com>
2022-04-29 20:43:19 +0300

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,47 @@
-$(document).ready(function() {
-    console.log( "ready!" );
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$('.btn-burger').on('click', function () {
+    $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.mobile-menu').fadeOut();
+});
+
+$('.community-slider').slick({
+    slidesToShow: 3,
+    dots: true,
+    appendArrows: '.community-slider-nav',
+    appendDots: '.community-slider-nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
+
+$('.news-slider').slick({
+    slidesToShow: 3,
+    dots: true,
+    appendArrows: '.news-slider-nav',
+    appendDots: '.news-slider-nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
+$('.reviews-slider').slick({
+    slidesToShow: 2,
+    dots: true,
+    appendArrows: '.reviews-slider-nav',
+    appendDots: '.reviews-slider-nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
+
+$('.partners-slider').slick({
+    slidesToShow: 8,
+    slidesToScroll: 2,
+    dots: true,
+    appendArrows: '.partners-slider-nav',
+    appendDots: '.partners-slider-nav',
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
