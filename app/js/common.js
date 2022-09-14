c18a33b6975f186bf27fcd5448421dd3098395ed
Viktoria <zaharova181114@gmail.com>
2022-09-14 14:55:13 +0300

    add page1

--- app/js/common.js
+++ app/js/common.js
@@ -171,3 +171,16 @@ $(window).on('load resize', function () {
 });
 // slick active
 
+
+
+$('.go_to').click(function (e) {
+    e.preventDefault();
+    var scroll_el = $(this).attr('href');
+    if ($(scroll_el).length !== 0) {
+        $('html, body').animate({
+            scrollTop: $(scroll_el).offset().top
+        }, 500);
+    }
+    return false;
+});
+
